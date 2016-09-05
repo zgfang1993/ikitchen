41f7c3e6a77cb84b7e4133442af98616c85d2da9
zhangguifang <zgfang1993@gmail.com>
2016-09-05 22:24:37 +0800

    add new page

--- www/js/app.js
+++ www/js/app.js
@@ -1,23 +1,18 @@
 // Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
+
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
 
     }
     if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
@@ -27,19 +22,27 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
   $stateProvider
 
-  // setup an abstract state for the tabs directive
   .state('tab', {  //下方导航栏
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html'
   })
 
-  .state('tab.dash', {
-    url: '/dash',
+  .state('tab.ikitchen', {
+    url: '/ikitchen',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
+      'tab-ikitchen': {
+        templateUrl: 'templates/tab-ikitchen.html',
+        controller: 'ikitchenCtrl'
+      }
+    }
+  })
+  .state('tab.ikitchen-popular', {
+    url: '/ikitchen/weekly_popular',
+    views: {
+      'tab-ikitchen': {
+        templateUrl: 'templates/ikitchen-popular.html',
+        controller: 'PopulCtrl'
       }
     }
   })
@@ -73,7 +76,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+
+  $urlRouterProvider.otherwise('/tab/ikitchen');
 
 });
